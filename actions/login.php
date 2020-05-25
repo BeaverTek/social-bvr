79ffc9711887a8c1610dcbdc7811462ba3b729cc
Hugo Sales <hugo@fc.up.pt>
2020-05-25 00:44:02 +0000

    Broken, but it's a start

--- actions/login.php
+++ actions/login.php
@@ -45,175 +45,48 @@ class LoginAction extends FormAction
      */
     protected function handle()
     {
-        if (common_is_real_login()) {
-            common_redirect(common_local_url('all', array('nickname' => $this->scoped->nickname)), 307);
-        }
-
-        return parent::handle();
-    }
-
-    /**
-     * Check the login data
-     *
-     * Determines if the login data is valid. If so, logs the user
-     * in, and redirects to the 'with friends' page, or to the stored
-     * return-to URL.
-     *
-     * @return void
-     */
-    protected function doPost()
-    {
-        // XXX: login throttle
-
-        $nickname = $this->trimmed('nickname');
-        $password = $this->arg('password');
-
-        $user = common_check_user($nickname, $password);
-
-        if (!$user instanceof User) {
-            // TRANS: Form validation error displayed when trying to log in with incorrect credentials.
-            throw new ServerException(_('Incorrect username or password.'));
-        }
-
-        // success!
-        if (!common_set_user($user)) {
-            // TRANS: Server error displayed when during login a server error occurs.
-            throw new ServerException(_('Error setting user. You are probably not authorized.'));
-        }
-
-        common_real_login(true);
-        $this->updateScopedProfile(); 
-
-        if ($this->boolean('rememberme')) {
-            common_rememberme($user);
-        }
-
-        $url = common_get_returnto();
-
-        if ($url) {
-            // We don't have to return to it again
-            common_set_returnto(null);
-            $url = common_inject_session($url);
+        if (!isset($_GET['code'])) {
+            common_redirect('https://login.hsales.me/auth/realms/SSO/' .
+                            'protocol/openid-connect/auth?scope=openid&client_id=social&response_type=code&redirect_uri='
+                            . urlencode(common_local_url('login')));
         } else {
-            $url = common_local_url('all',
-                                    array('nickname' => $this->scoped->nickname));
-        }
-
-        common_redirect($url, 303);
-    }
-
-    function showScripts()
-    {
-        parent::showScripts();
-        $this->autofocus('nickname');
-    }
-
-    /**
-     * Title of the page
-     *
-     * @return string title of the page
-     */
-    function title()
-    {
-        // TRANS: Page title for login page.
-        return _('Login');
-    }
-
-    /**
-     * Core of the display code
-     *
-     * Shows the login form.
-     *
-     * @return void
-     */
-    function showContent()
-    {
-        $this->elementStart('form', array('method' => 'post',
-                                          'id' => 'form_login',
-                                          'class' => 'form_settings',
-                                          'action' => common_local_url('login')));
-        $this->elementStart('fieldset');
-        // TRANS: Form legend on login page.
-        $this->element('legend', null, _('Login to site'));
-        $this->elementStart('ul', 'form_data');
-        $this->elementStart('li');
-        // TRANS: Field label on login page.
-        $this->input('nickname', _('Username or email address'));
-        $this->elementEnd('li');
-        $this->elementStart('li');
-        // TRANS: Field label on login page.
-        $this->password('password', _('Password'));
-        $this->elementEnd('li');
-        $this->elementStart('li');
-        // TRANS: Checkbox label label on login page.
-        $this->checkbox('rememberme', _('Remember me'), false,
-                        // TRANS: Checkbox title on login page.
-                        _('Automatically login in the future; ' .
-                          'not for shared computers!'));
-        $this->elementEnd('li');
-        $this->elementEnd('ul');
-        // TRANS: Button text for log in on login page.
-        $this->submit('submit', _m('BUTTON','Login'));
-        $this->hidden('token', common_session_token());
-        $this->elementEnd('fieldset');
-        $this->elementEnd('form');
-        $this->elementStart('p');
-        $this->element('a', array('href' => common_local_url('recoverpassword')),
-                       // TRANS: Link text for link to "reset password" on login page.
-                       _('Lost or forgotten password?'));
-        $this->elementEnd('p');
-    }
-
-    /**
-     * Instructions for using the form
-     *
-     * For "remembered" logins, we make the user re-login when they
-     * try to change settings. Different instructions for this case.
-     *
-     * @return void
-     */
-    protected function getInstructions()
-    {
-        if (common_logged_in() && !common_is_real_login() &&
-            common_get_returnto()) {
-            // rememberme logins have to reauthenticate before
-            // changing any profile settings (cookie-stealing protection)
-            // TRANS: Form instructions on login page before being able to change user settings.
-            return _('For security reasons, please re-enter your ' .
-                     'user name and password ' .
-                     'before changing your settings.');
-        } else {
-            // TRANS: Form instructions on login page.
-            $prompt = _('Login with your username and password.');
-            if (!common_config('site', 'closed') && !common_config('site', 'inviteonly')) {
-                $prompt .= ' ';
-                // TRANS: Form instructions on login page. This message contains Markdown links in the form [Link text](Link).
-                // TRANS: %%action.register%% is a link to the registration page.
-                $prompt .= _('Don\'t have a username yet? ' .
-                             '[Register](%%action.register%%) a new account.');
+            if (!isset($_SESSION['oauth2state']) && !empty($_GET['session_state'])) {
+                $_SESSION['oauth2state'] = $_GET['session_state'];
             }
-            return $prompt;
+
+            $http = new HTTPClient('https://login.hsales.me/auth/realms/SSO/protocol/openid-connect/token', 'POST');
+            $http->addPostParameter(['code' => $_GET['code'],
+                                     'grant_type' => 'authorization_code',
+                                     'client_id' => 'social',
+                                     'redirect_uri' => common_local_url('login')]);
+
+            $response = json_decode($http->send()->getBody());
+
+            $jwt_data = json_decode(base64_decode(explode('.', $response->access_token)[1]));
+
+            $user = User::getKV($jwt_data->preferred_username);
+
+            if (empty($user)) {
+                $user = User::insert(
+                    ['nickname' => $jwt_data->preferred_username,
+                     // 'email' => $jwt_data->email,
+                     'password' => common_random_hexstr(32),
+                     'fullname' => $jwt_data->given_name . ' ' . $jwt_data->family_name,
+                     // 'email_confirmed' => $jwt_data->email_verified,
+                    ]);
+            }
+
+            var_dump($user);
+
+            // common_set_user($user);
+            // common_real_login(true);
+            // $this->updateScopedProfile();
         }
-    }
 
-    /**
-     * A local menu
-     *
-     * Shows different login/register actions.
-     *
-     * @return void
-     */
-    function showLocalNav()
-    {
-        $nav = new LoginGroupNav($this);
-        $nav->show();
-    }
+        // if (common_is_real_login()) {
+        //     common_redirect(common_local_url('all', array('nickname' => $this->scoped->nickname)), 307);
+        // }
 
-    function showNoticeForm()
-    {
-    }
-
-    function showProfileBlock()
-    {
+        // return parent::handle();
     }
 }
